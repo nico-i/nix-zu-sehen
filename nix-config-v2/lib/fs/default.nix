fdc3f3ad47f2e2ffa044fbdcc5fc59dcae214ce1
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-11 17:57:06 +0200

    fix: added missing export

--- nix-config-v2/lib/fs/default.nix
+++ nix-config-v2/lib/fs/default.nix
@@ -2,6 +2,7 @@
 	inputs,
 	lib
 }: {
+	getFileName = import ./getFileName.nix;
 	listDirsInDir = import ./listDirsInDir.nix { inherit inputs; };
 	listFilesInDir = import ./listFilesInDir.nix { inherit lib; };
 }
