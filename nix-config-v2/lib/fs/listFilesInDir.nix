3bfc2ee50770a3ae4f21429a5dd13da30f9e66fd
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-11 17:50:57 +0200

    fix: wrong builtin func

--- nix-config-v2/lib/fs/listFilesInDir.nix
+++ nix-config-v2/lib/fs/listFilesInDir.nix
@@ -1,5 +1,5 @@
 /* Create a list of files (paths) in a given directory */
 { lib }: { dir }: 
-    if builtins.pathExists dir && lib.filesystem.pathType dir
+    if builtins.pathExists dir && lib.filesystem.pathIsDirectory dir
     then map (fname: dir + "/${fname}") (builtins.attrNames (builtins.readDir dir))
     else throw "listFilesInDir: `dir` must be an existing directory"
