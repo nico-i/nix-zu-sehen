8ce6552bc29cfbbca3c4415272805f4d70f72592
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-11 15:14:21 +0200

    fix: added parenthesis

--- nix-config-v2/lib/build/default.nix
+++ nix-config-v2/lib/build/default.nix
@@ -1,11 +1,5 @@
-{
-    inputs
-}:
-let
-    mkNixOSSystem = import ./mkNixOSSystem.nix { inherit inputs; };
-    mkHomeConfig = import ./mkHomeConfig.nix { inherit inputs; };
-    mkDarwinSystem = import ./mkDarwinSystem.nix { inherit inputs; };
-in
-    {
-        inherit mkNixOSSystem mkHomeConfig mkDarwinSystem;
-    }
+{ inputs }: {
+	mkNixOSSystem = (import ./mkNixOSSystem.nix { inherit inputs; });
+	mkHomeConfig = (import ./mkHomeConfig.nix { inherit inputs; });
+	mkDarwinSystem = (import ./mkDarwinSystem.nix { inherit inputs; });
+}
