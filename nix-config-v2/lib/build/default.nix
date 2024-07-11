4d9a337492dbb71e7a9f81b724f8edb554af0611
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-11 15:11:05 +0200

    fix: export error

--- nix-config-v2/lib/build/default.nix
+++ nix-config-v2/lib/build/default.nix
@@ -1,7 +1,11 @@
 {
     inputs
 }:
-{
+let
     mkNixOSSystem = import ./mkNixOSSystem.nix { inherit inputs; };
     mkHomeConfig = import ./mkHomeConfig.nix { inherit inputs; };
-}
+    mkDarwinSystem = import ./mkDarwinSystem.nix { inherit inputs; };
+in
+    {
+        inherit mkNixOSSystem mkHomeConfig mkDarwinSystem;
+    }
