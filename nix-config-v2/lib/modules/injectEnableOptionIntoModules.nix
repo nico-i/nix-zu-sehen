485829b6c7ffd9c308be0f42355cd5807f8d6537
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-11 17:45:58 +0200

    fix: typo

--- nix-config-v2/lib/modules/injectEnableOptionIntoModules.nix
+++ nix-config-v2/lib/modules/injectEnableOptionIntoModules.nix
@@ -1,16 +1,18 @@
 { lib, helperLib }:
-    { modulesDirPath, customConfig, customConfigName}: []
-        # let
-        #     moduleCategoryName = if !(builtins.isPath modulesDirPath)
-        #         then throw "injectEnableOptionIntoModules: `modulesDirPath` must be a path"
-        #         else helperLib.fs.getFileName modulesDirPath;
-        #     modulePaths = helperLib.fs.listFilesInDir modulesDirPath;
-        #     modules = if builtins.isList modulePaths
-        #         then modulePaths
-        #         else throw "injectEnableOptionIntoModules: `modulesDirPath` must be a directory";
-        # in
-        #     map (modulePath: helperLib.modules.injectEnableOptionIntoModule 
-        #             {
-        #                 inherit modulePath lib customConfig customConfigName moduleCategoryName;
-        #             }
-        #         ) modules
+    { modulesDirPath, customConfig, customConfigName}:
+        let
+            moduleCategoryName = if !(builtins.isPath modulesDirPath)
+                then throw "injectEnableOptionIntoModules: `modulesDirPath` must be a path"
+                else helperLib.fs.getFileName modulesDirPath;
+
+            modulePaths = helperLib.fs.listFilesInDir { dir = modulesDirPath; };
+
+            modules = if !(builtins.isList modulePaths)
+                then throw "injectEnableOptionIntoModules: `modulesDirPath` must be a directory"
+                else modulePaths;
+        in
+            map (modulePath: helperLib.modules.injectEnableOptionIntoModule 
+                    {
+                        inherit modulePath lib customConfig customConfigName moduleCategoryName;
+                    }
+                ) modules
