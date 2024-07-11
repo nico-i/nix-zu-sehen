10da8e06833785866911a093aede69c9d2edd10e
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-11 17:49:42 +0200

    fix: added lib var

--- nix-config-v2/lib/default.nix
+++ nix-config-v2/lib/default.nix
@@ -1,15 +1,15 @@
 {
 	inputs
-} : 
+} :
 let
 	helperLib = (import ./default.nix) { inherit inputs; };
+	lib = inputs.nixpkgs.lib;
 in
 	{
 		build = (import ./build) { inherit inputs; };
 		nixos = (import ./nixos) { inherit inputs; };
 		fs = (import ./fs) { inherit inputs lib; };
 		modules = (import ./modules) {  
-				inherit helperLib; 
-				lib = inputs.nixpkgs.lib;
+				inherit helperLib lib;
 			};
 	}
