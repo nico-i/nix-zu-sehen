c5ccf62561b4395c92ab494327e97e38b900e148
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-11 17:39:10 +0200

    chore: test

--- nix-config-v2/modules/nixos/default.nix
+++ nix-config-v2/modules/nixos/default.nix
@@ -6,7 +6,7 @@
 }: let
   cfg = config.nixosModulesConfig;
 
-  systemModules = helperLib.modules.injectEnableOptionIntoModules {}
+  systemModules = helperLib.modules.injectEnableOptionIntoModules {};
   
 in {
   imports = []
