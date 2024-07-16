21cce1be9e2144cae71c075e2ee9b51f083d7952
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-16 11:54:43 +0200

    feat: auto-set hostname

--- nix-config-v2/lib/build/mkNixOSSystem.nix
+++ nix-config-v2/lib/build/mkNixOSSystem.nix
@@ -6,19 +6,25 @@ let
   outputs = inputs.self.outputs;
 in 
 { nixosCfgPath }:
-	if !(builtins.isPath nixosCfgPath)
-	then
-		throw "mkNixOSSystem: `nixosCfgPath` must be a path"
-	else
-		inputs.nixpkgs.lib.nixosSystem {
-			# used to pass things to the NixOS configuration
-			specialArgs = {
-				inherit inputs outputs helperLib;
-			};
-			# all modules that are necessary for building the system
-			modules = [
-				nixosCfgPath
-				outputs.nixosModules.default
-			];
-		}
+let
+	hostName = 
+		if !(builtins.isPath nixosCfgPath)
+		then throw "mkNixOSSystem: `nixosCfgPath` must be a path"
+		else builtins.baseNameOf (builtins.dirOf nixosCfgPath);
+in
+	inputs.nixpkgs.lib.nixosSystem {
+		# used to pass things to the NixOS configuration
+		specialArgs = {
+			inherit inputs outputs helperLib;
+		};
+		# all modules that are necessary for building the system
+		modules = [
+			nixosCfgPath
+			outputs.nixosModules.default
+			{
+				# set the hostname to the name of the dir containing the configuration
+				networking.hostName = hostName;
+			}
+		];
+	}
 
