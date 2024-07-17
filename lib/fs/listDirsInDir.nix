48f94311358d6c7903a0ee9b20e7dc37323a2933
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-17 12:30:47 +0200

    fix: used correct lib

--- lib/fs/listDirsInDir.nix
+++ lib/fs/listDirsInDir.nix
@@ -1,5 +1,5 @@
 /* Create a list of directories in a given directory */
 { inputs }: { dir }: 
-	if builtins.pathExists dir && builtins.isDir dir
+        if builtins.pathExists dir && lib.filesystem.pathIsDirectory dir
         then inputs.nixpkgs.lib.filterAttrs (name: value: value == "directory") (builtins.readDir dir)
         else throw "listDirsInDir: `dir` must be an existing directory"
