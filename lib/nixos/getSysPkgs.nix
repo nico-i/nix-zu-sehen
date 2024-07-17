f9645aef47b7861357cea32ec86330e70fd2f965
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-17 13:32:59 +0200

    fix: getSysPkgs call

--- lib/nixos/getSysPkgs.nix
+++ lib/nixos/getSysPkgs.nix
@@ -1,5 +1,5 @@
 /** Retrieve all system-specific packages for a given system. */
-{ inputs, system }:
+{ inputs }: { system }:
     if !(builtins.isString system)
     then throw "getSysPkgs: `system` must be a string"
     else inputs.nixpkgs.legacyPackages.${system}
