df084fc9517df681a82aa4121eab7e629fd9f01b
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-17 14:56:28 +0200

    feat: added monitor config

--- hosts/mbvm/users/nico/home.nix
+++ hosts/mbvm/users/nico/home.nix
@@ -11,6 +11,16 @@
 		customization = {
 			hyprland.enable = lib.mkDefault true;
 		};
+
+        monitors = [
+            {
+				# MacBook 14"
+				name = "DP-3";
+				width = 3024;
+				height = 1964;
+				refreshRate = 60;
+			}
+        ];
 	};
 
     home.stateVersion = "24.05"; # Do not change
