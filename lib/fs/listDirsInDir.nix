dd323ea82d11028435a573396185b9cb197e751b
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-17 12:42:44 +0200

    feat: added `withPath` param

--- lib/fs/listDirsInDir.nix
+++ lib/fs/listDirsInDir.nix
@@ -1,5 +1,11 @@
 /* Create a list of directories in a given directory */
-{ inputs, lib }: { dir }: 
-        if builtins.pathExists dir && lib.filesystem.pathIsDirectory dir
-        then lib.attrNames (inputs.nixpkgs.lib.filterAttrs (name: value: value == "directory") (builtins.readDir dir))
-        else throw "listDirsInDir: `dir` must be an existing directory"
+{ inputs, lib, withPath ? true }: { dir }: 
+let
+        containedDirs = 
+                if builtins.pathExists dir && lib.filesystem.pathIsDirectory dir
+                then lib.attrNames (inputs.nixpkgs.lib.filterAttrs (name: value: value == "directory") (builtins.readDir dir))
+                else throw "listDirsInDir: `dir` must be an existing directory";
+in
+        if withPath
+        then builtins.map (dirName: "${dir}/${dirName}") containedDirs
+        else containedDirs
