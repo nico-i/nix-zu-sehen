cfcff8ad787245ecee720eeaaed61e319e50a95e
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-17 13:07:08 +0200

    feat: better validation

--- lib/fs/getFileName.nix
+++ lib/fs/getFileName.nix
@@ -1,5 +1,5 @@
 /* Get the name of the target file at the target path */
 { path }:
-    if builtins.pathExists path 
+    if builtins.pathExists path && builtins.isPath path
 	then (builtins.head (builtins.split "\\." (baseNameOf path)))
-    else throw "getFileName: `path` must lead to an existing file, got: '${toString path}'"
+    else throw "getFileName: `path` must be a path and lead to an existing file, got: '${toString path}'"
