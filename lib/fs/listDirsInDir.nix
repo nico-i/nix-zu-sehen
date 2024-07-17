f96ec6a4378c93619d79990fe01e1c1c1b205541
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-17 12:37:26 +0200

    fix: return list not set

--- lib/fs/listDirsInDir.nix
+++ lib/fs/listDirsInDir.nix
@@ -1,5 +1,5 @@
 /* Create a list of directories in a given directory */
 { inputs, lib }: { dir }: 
         if builtins.pathExists dir && lib.filesystem.pathIsDirectory dir
-        then inputs.nixpkgs.lib.filterAttrs (name: value: value == "directory") (builtins.readDir dir)
+        then lib.attrNames (inputs.nixpkgs.lib.filterAttrs (name: value: value == "directory") (builtins.readDir dir))
         else throw "listDirsInDir: `dir` must be an existing directory"
