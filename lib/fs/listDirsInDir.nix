50d4d3b417e4f510357fdffc52a2d41354636034
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-17 13:15:41 +0200

    fix: correct dir paths

--- lib/fs/listDirsInDir.nix
+++ lib/fs/listDirsInDir.nix
@@ -1,11 +1,5 @@
 /* Create a list of directories in a given directory */
-{ inputs, lib, withPath ? true }: { dir }: 
-let
-        containedDirs = 
-                if builtins.pathExists dir && lib.filesystem.pathIsDirectory dir
-                then lib.attrNames (inputs.nixpkgs.lib.filterAttrs (name: value: value == "directory") (builtins.readDir dir))
-                else throw "listDirsInDir: `dir` must be an existing directory";
-in
-        if withPath
-        then builtins.map (dirName: "${dir}/${dirName}") containedDirs
-        else containedDirs
+{ inputs, lib }: { dir }: 
+        if builtins.pathExists dir && lib.filesystem.pathIsDirectory dir
+        then lib.attrNames (inputs.nixpkgs.lib.filterAttrs (name: value: value == "directory") (builtins.readDir dir))
+        else throw "listDirsInDir: `dir` must be an existing directory, got: '${toString dir}'"
