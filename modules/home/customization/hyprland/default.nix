cb4af7eb901cbb73f2d80cfc505e916923387ccc
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-18 09:18:36 +0200

    feat: improved dynamic binds

--- modules/home/customization/hyprland/default.nix
+++ modules/home/customization/hyprland/default.nix
@@ -7,104 +7,104 @@
 	osConfig,
 	...
 }: {
-    imports = [
-        ./monitors.nix
-    ];
+	imports = [
+		./monitors.nix
+	];
 
 	config = {
-        home.packages = with pkgs; [
-            # screenshot tools
-            grim
-            slurp
-            # clipboard manager with wayland support
-            wl-clipboard
-            # network manager tray icon
-            networkmanagerapplet
-        ];
+		home.packages = with pkgs; [
+			# screenshot tools
+			grim
+			slurp
+			# clipboard manager with wayland support
+			wl-clipboard
+			# network manager tray icon
+			networkmanagerapplet
+		];
 
-        customHomeConfig = {
-            customization = {
-                tofi.enable = true;
-                waybar.enable = true;
-                xremap.enable = true;
-            };
-            dev.kitty.enable = true;
-        };
+		customHomeConfig = {
+			customization = {
+				tofi.enable = true;
+				waybar.enable = true;
+				xremap.enable = true;
+			};
+			dev.kitty.enable = true;
+		};
 
-        wayland.windowManager.hyprland = {
-            enable = true;
-            settings = {
-                # https://wiki.hyprland.org/Configuring/Binds/ for more
-                bind = [
-                    "$mainMod, return, exec, kitty"
-                    "$mainMod, Q, killactive,"
-                    "$mainMod SHIFT, M, exit,"
-                    "$mainMod SHIFT, F, togglefloating,"
-                    "$mainMod, F, fullscreen,"
-                    "$mainMod, G, togglegroup,"
+		wayland.windowManager.hyprland = {
+			enable = true;
+			settings = {
+				# https://wiki.hyprland.org/Configuring/Binds/ for more
+				bind = [
+					"$mainMod, return, exec, kitty"
+					"$mainMod, Q, killactive,"
+					"$mainMod SHIFT, M, exit,"
+					"$mainMod SHIFT, F, togglefloating,"
+					"$mainMod, F, fullscreen,"
+					"$mainMod, G, togglegroup,"
 
-                    # vim navigation binds
-                    "$mainMod, h, movefocus, l"
-                    "$mainMod, l, movefocus, r"
-                    "$mainMod, k, movefocus, u"
-                    "$mainMod, j, movefocus, d"
+					# vim navigation binds
+					"$mainMod, h, movefocus, l"
+					"$mainMod, l, movefocus, r"
+					"$mainMod, k, movefocus, u"
+					"$mainMod, j, movefocus, d"
 
-                    "$mainMod SHIFT, h, movewindow, l"
-                    "$mainMod SHIFT, l, movewindow, r"
-                    "$mainMod SHIFT, k, movewindow, u"
-                    "$mainMod SHIFT, j, movewindow, d"
-                ]
-                ++ map (n: "$mainMod SHIFT, ${toString n}, movetoworkspace, ${toString (
-                    if n == 0
-                    then 10
-                    else n
-                )}") [1 2 3 4 5 6 7 8 9 0]
-                ++ map (n: "$mainMod, ${toString n}, workspace, ${toString (
-                    if n == 0
-                    then 10
-                    else n
-                )}") [1 2 3 4 5 6 7 8 9 0];
+					"$mainMod SHIFT, h, movewindow, l"
+					"$mainMod SHIFT, l, movewindow, r"
+					"$mainMod SHIFT, k, movewindow, u"
+					"$mainMod SHIFT, j, movewindow, d"
+				]
+				++ map 
+                    (numberKey: 
+                        let n = if numberKey == 0 then 10 else numberKey;
+                        in  
+                        [
+                            "$mainMod, ${toString numberKey}, workspace, ${toString n}"
+                            "$mainMod SHIFT, ${toString numberKey}, movetoworkspace, ${toString n}"
+                        ]
+                    )
+					[1 2 3 4 5 6 7 8 9 0];
 
 
-                monitor =
-                    map
-                    (
-                        m: let
-                        resolution = "${toString m.width}x${toString m.height}@${toString m.refreshRate}";
-                        position = "${toString m.x}x${toString m.y}";
-                        in "${m.name},${
+				monitor =
+					map
+					(
+						m: let
+						resolution = "${toString m.width}x${toString m.height}@${toString m.refreshRate}";
+						position = "${toString m.x}x${toString m.y}";
+						in "${m.name},${
                         if m.enabled
                         then "${resolution},${position},1"
                         else "disable"
                         }"
-                    )
-                    (config.customHomeConfig.monitors);
+					)
+					(config.customHomeConfig.monitors);
 
-                    workspace = map (m: "${m.name},${m.workspace}")
-                        (lib.filter (m: m.enabled && m.workspace != null) config.customHomeConfig.monitors);
-                    
-                     input = {
-                        kb_layout = "us";
-                        kb_variant = "";
-                        kb_model = "";
-                        kb_options = "";
+					workspace = map (m: "${m.name},${m.workspace}")
+						(lib.filter (m: m.enabled && m.workspace != null) config.customHomeConfig.monitors);
 
-                        kb_rules = "";
+						input = {
+							kb_layout = "us";
+							kb_variant = "";
+							kb_model = "";
+							kb_options = "";
 
-                        follow_mouse = 1;
+							kb_rules = "";
 
-                        repeat_rate = 40;
-                        repeat_delay = 250;
-                        force_no_accel = true;
+							follow_mouse = 1;
 
-                        sensitivity = 0.0; # -1.0 - 1.0, 0 means no modification.
-                    };
+							repeat_rate = 40;
+							repeat_delay = 250;
+							force_no_accel = true;
 
-                     misc = {
-                        enable_swallow = true;
-                        force_default_wallpaper = 0;
-                    };
-            };
-        };
-    };    
+							sensitivity = 0.0; # -1.0 - 1.0, 0 means no modification.
+					};
+
+						misc = {
+							enable_swallow = true;
+							force_default_wallpaper = 0;
+					};
+			};
+		};
+		}; 
 }
