f8582472be8e506e5da7c0f34584ffddbb28123e
Nico Ismaili <nico.ismaili@aoe.com>
2025-07-29 18:17:45 +0200

    chore: fix greetd systemd config

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -103,17 +103,19 @@ in
             user = "greeter";
           };
         };
-        serviceConfig = {
-          Type = "idle";
-          StandardInput = "tty";
-          StandardOutput = "tty";
-          StandardError = "journal"; # Without this errors will spam on screen
-          # Without these bootlogs will spam on screen
-          TTYReset = true;
-          TTYVHangup = true;
-          TTYVTDisallocate = true;
-        };
       };
+    };
+
+    # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+    systemd.services.greetd.serviceConfig = {
+      Type = "idle";
+      StandardInput = "tty";
+      StandardOutput = "tty";
+      StandardError = "journal"; # Without this errors will spam on screen
+      # Without these bootlogs will spam on screen
+      TTYReset = true;
+      TTYVHangup = true;
+      TTYVTDisallocate = true;
     };
 
     networking = {
