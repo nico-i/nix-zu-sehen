976b9eb405e82a8d6431c7c34a58251b193e0642
Nico Ismaili <nico.ismaili@aoe.com>
2025-07-29 14:56:46 +0200

    chore: fix typo

--- modules/home/ui/launcher.nix
+++ modules/home/ui/launcher.nix
@@ -5,6 +5,15 @@
     # https://github.com/philj56/tofi
     enable = true;
     # See https://github.com/philj56/tofi/blob/master/doc/config for a full list of options.
-    font = "monospace";
+    settings = {
+      # Set the font used in tofi
+      font = "monospace";
+      # Set the size of the font
+      fontSize = 12;
+      # Set the color scheme for tofi
+      colorScheme = "default";
+      # Set the maximum number of results displayed
+      maxResults = 10;
+    };
   };
 }
