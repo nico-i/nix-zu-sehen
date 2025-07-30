465f87dfa7eba1acf107450c7c691d6582f4a21f
Nico Ismaili <nico.ismaili@aoe.com>
2025-07-30 12:26:14 +0200

    chore: rm alacritty conf for now

--- modules/home/cli/terminalEmulator.nix
+++ modules/home/cli/terminalEmulator.nix
@@ -4,13 +4,5 @@
     # Alacritty - Terminal Emulator
     # https://github.com/alacritty/alacritty
     enable = true;
-    settings = {
-      # See `man 5 alacritty` for more information
-      window = {
-        opacity = 0.9;
-        dynamic_padding = true;
-        blur = true;
-      };
-    };
   };
 }
