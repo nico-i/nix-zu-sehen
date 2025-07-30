be1b76b4319449419b20493c80994e33bc2f057d
Nico Ismaili <nico.ismaili@aoe.com>
2025-07-30 14:45:29 +0200

    chore: reverted login

--- modules/nixos/ui/desktop.nix
+++ modules/nixos/ui/desktop.nix
@@ -11,11 +11,23 @@
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.wlgreet}/bin/wlgreet --command Hyprland";
-          user = "greeter";
-        };
+            command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd Hyprland";
+            user = "greeter";
+          };
       };
     };
+
+    # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+    systemd.services.greetd.serviceConfig = {
+      Type = "idle";
+      StandardInput = "tty";
+      StandardOutput = "tty";
+      StandardError = "journal"; # Without this errors will spam on screen
+      # Without these bootlogs will spam on screen
+      TTYReset = true;
+      TTYVHangup = true;
+      TTYVTDisallocate = true;
+
 
     xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     xdg.portal.enable = true;
